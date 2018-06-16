0cb5200c85cfd0ac914f6d2233b45739ee9dd33d
Jason <jason@deadtreepages.com>
2018-06-16 19:05:41 +0000

    Add wait_results and docstrings.

--- asynner/loopworker.py
+++ asynner/loopworker.py
@@ -2,7 +2,10 @@
 Run an asyncio loop in it's own thread for scheduling awaitables from
 within Pyramid views.
 
-Adds a request property `loop` that returns the loop running
+This adds request property `loop` and request methods `wait_for`,
+`wait_results`, and `await`.
+
+The `loop` property returns the loop running
 in it's own thread. Use this loop with `asyncio.run_coroutine_threadsafe`
 to run awaitables. For example::
     
@@ -10,13 +13,30 @@ to run awaitables. For example::
     from concurrent import futures
     
     def my_view(request):
-        future = asyncio.run_coroutine_threadsafe(my_coroutine(), request.loop)
-        done, not_done = futures.wait(running, timeout=3)
+        running = asyncio.run_coroutine_threadsafe(my_coroutine(), request.loop)
+        done, not_done = futures.wait([running, ...], timeout=3)
         results = [ftr.result() for ftr in done]
         return {'done': results}
+    
 
-A request method is added that allows running a single awaitable and
-blocking until it's results are complete or timeout passes.
+The `wait_for` method reduces the above boiler plate by waiting for the
+futures. Example usage::
+
+    def my_view(request):
+        done, not_done = request.wait_for([my_coroutine(), ...])
+        results = [ftr.result() for ftr in done]
+        return {'done': results}
+        
+The `wait_results` method reduces the boiler plate further by returning
+the results after waiting. This is useful if you don't care about
+unfinished tasks. Example usage::
+
+    def my_view(request):
+        results = request.wait_for([my_coroutine(), ...])
+        return {'done': results}
+
+The `await` method runs a single awaitable and
+blocks until it's results are complete or timeout passes.
 Example usage::
     
     from loopworker import AwaitableTimeout
@@ -31,7 +51,7 @@ Example usage::
 """
 import asyncio
 import threading
-from typing import Awaitable
+from typing import Awaitable, List, Optional, Tuple
 
 
 def includeme(config):
@@ -49,6 +69,69 @@ def includeme(config):
         run_awaitable,
         'await',
     )
+    
+    config.add_request_method(
+        wait_for_awaitables,
+        'wait_for'
+    )
+    
+    config.add_request_method(
+        wait_for_results,
+        'wait_results'
+    )
+    
+    
+def wait_for_awaitables(
+    request,
+    awaitables: List[Awaitable],
+    timeout: int = 3,
+    cancel_unfinished: bool = True
+) -> Tuple[List[asyncio.Future], List[asyncio.Future]]:
+    """
+    Wait for awaitables to complete and return tuple of finished and
+    unfinished futures.
+    
+    If cancel_unfinished is True, any awaitables that are not complete
+    by the timeout are cancelled.
+    """
+    running = [
+        asyncio.run_coroutine_threadsafe(
+            awaitable,
+            loop=request.loop
+        )
+        for awaitable in awaitables
+    ]
+    
+    done, not_done = futures.wait(running, timeout=timeout)
+    
+    if cancel_unfinished:
+        for ftr in not_done:
+            ftr.cancel()
+    
+    return done, not_done
+
+
+def wait_for_results(
+    request,
+    awaitables: List[Awaitable],
+    timeout: int = 3,
+    cancel_unfinished: bool = True
+) -> List[Any]:
+    """
+    Wait for awaitables to complete and return list of results from
+    finished tasks.
+    
+    Note that the returned results are *not* in the same order as
+    the awaitables list argument.
+    
+    If cancel_unfinished is True, any awaitables that are not complete
+    by the timeout are cancelled.
+    """
+    done, not_done = wait_for_awaitables(
+        request, awaitables, timeout, cancel_unfinished
+    )
+    
+    return [ftr.result() for ftr in done]
 
 
 def run_awaitable(request, coroutine: Awaitable, timeout: int = 3):
